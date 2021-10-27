dbf5e141ae46ed5dcfc40ae6e110d7f369f004b2
Giwan Persaud <giwan.persaud@gmail.com>
2021-10-27 18:16:55 +0200

    updated test script

--- test-server.js
+++ test-server.js
@@ -1,4 +1,5 @@
 const https = require("https");
+const app = require("./api/index.js");
 
 const handler = () => {
     const url = new URL(process.argv[2]);
@@ -13,13 +14,18 @@ const handler = () => {
         const headers = newRes.headers;
         // console.log("headers: ", headers);
 
-        const xframeoptions = headers["X-Frame-Options"] || headers["x-frame-options"];
-        if (xframeoptions && ["DENY", "SAMEORIGIN"].includes(xframeoptions)) {
-            console.log("this page does not allow iframing: ", xframeoptions); 
-        } else {
-            console.log("page allows for iframing: ", xframeoptions);
-        }
+        // const xframeoptions = headers["X-Frame-Options"] || headers["x-frame-options"];
+        // if (xframeoptions && ["DENY", "SAMEORIGIN"].includes(xframeoptions)) {
+        //     console.log("this page does not allow iframing: ", xframeoptions); 
+        // } else {
+        //     console.log("page allows for iframing: ", xframeoptions);
+        // }
+
     });
+    newReq.query = {
+        url: "www.mathsisfun.com/place-value.html"
+    }
+    app(newReq, { setHeader: () => {}, json: (data) => console.log("json response: ", data) });
 
     newReq.end();
 };
